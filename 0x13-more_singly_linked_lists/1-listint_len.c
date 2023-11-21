afc6fb221433820aaeb7ff6346c0386e773f28e3
Lerato Seoke <seokemotlalepula@gmail.com>
2023-11-21 14:33:07 -0800

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a
- * linked list
- * @h:: linked list of type listint_t to traverse
- * Return: number of nodes
+ * listint_len - the number of elements in a linked listint_t list
+ * @h: head pointer
+ * Return: the number of elements
  */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		num++;
 		h = h->next;
