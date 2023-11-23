552141f97ecf5c84faa771cfb25f21ff5665deed
Lerato Seoke <seokemotlalepula@gmail.com>
2023-11-23 03:06:08 -0800

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * get_bit - return the value of a bit at a given
- * index
+ * get_bit - return the value of a bit at a given index
  * @n: unsigned long int input
- * #index: index of the bit
+ * @index: index of the bit
  * Return: value of the bit
  */
 int get_bit(unsigned long int n, unsigned int index)
